72807972d12188c866bc0bb3c61e27fd0c24c8f1
Nightowlam <nightowl3.00am@gmail.com>
2022-11-07 23:13:32 +0600

    initial setup and mongodb connect

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bplzh34.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    const serviceCollection = client.db("opinion-plug").collection("services");
+  } finally {
+  }
+};
+dbConnect().catch((error) => console.log(error.message));
+
+app.get("/", (req, res) => {
+  res.send("Opinion plug server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
